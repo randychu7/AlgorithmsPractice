0247aa4f4a5fe616846c42bafa64a04835f3d224
Randy Chu <randychu7@gmail.com>
2023-05-11 12:02:51 -0500

    practice 1

--- index.js
+++ index.js
@@ -32,79 +32,136 @@
 
 //JS ES7 Classes and Constructor
 
-   class Human {
-        gender = 'male';
+//    class Human {
+//         gender = 'male';
     
-    printGender = () => {
-        console.log(this.gender)
-    }
-}
+//     printGender = () => {
+//         console.log(this.gender)
+//     }
+// }
    
-    class Person extends Human {
+//     class Person extends Human {
       
-        name = 'max';
-        gender = 'female';
+//         name = 'max';
+//         gender = 'female';
        
 
-        printName = () => {
-                console.log(this.name);
-            }
+//         printName = () => {
+//                 console.log(this.name);
+//             }
+//         }
+    
+    
+//     const person = new Person();
+//     person.printName();
+//     person.printGender();
+
+
+//     // Spread Operator / Rest
+
+//     //Spread Operator is used to split up array elements OR object properties
+
+//     // const newArray = [...oldArray, 1, 2];
+//     // const newObject = {...oldObject, newProp:5}
+
+//     //Rest Operator Used to merge a list of functions arguments into an array
+
+//     sortArgs = (...args) =>{
+//         return args.sort()
+//     }
+
+//     const numbers =[1,2,3,4];
+//     const newNumbers = [...numbers,5,6,7];
+
+//     console.log(newNumbers)
+
+// const filter =(...args) =>{
+//     return args.filter(el => el === 1);
+// }
+
+// console.log(filter(1,2,3))
+
+// //Destructuring extract array elements or objext properties and store them in variables
+
+// const number = [1,2,3];
+
+// [num1, ,num3] = number;
+
+// console.log(num1, num3)
+
+// //Array Method
+
+// const numbers1 = [1,2,3,4];
+
+// const doubleNumArray = numbers.map((num) => {
+//     return num * 2;
+// })
+
+// console.log(numbers1);
+
+// console.log(doubleNumArray)
+
+// //Your task is to add the missing logic to a transformToObjects() function that transforms a list of numbers into a list of JavaScript objects.     
+// //For the provided input [1, 2, 3] the transformToObjects() function should return [{val: 1}, {val: 2}, {val: 3}].
+// function transformToObjects(numberArray) {
+//     return numberArray.map((num) => {
+//       return { val: num };
+//     });
+//   }
+
+// const candle = [1,2,3,3,3,3];
+
+
+// function birthdayCakeCandles(candles) {
+//     let highest = 0;
+//     let sortCandles = candles.sort((a,b) => b-a);
+//     let tallestCandel = sortCandles[0];
+//     for(let i = 0; i < sortCandles.length; i++){
+//         if(sortCandles[i] === sortCandles[0]){
+//             highest ++;
+//         }
+//     }
+//     console.log(highest)
+//     return highest;
+
+// }
+
+// console.log(birthdayCakeCandles(candle));
+
+
+// var twoSum = function(nums, target) {
+//     for(let i = 0; i < nums.length; i++){
+//         for(let j = i+1; j < nums.length; j++){
+//             if(nums[i] + nums[j] === target){
+//                 return [i,j];
+//             }
+//         }
+//     }
+// };
+
+// console.log(twoSum([3,2,3], 6));
+
+const bracket = "{}[]()"
+
+let stack = [];
+var isValid = function(s) {
+    for(let i = 0; i < s.length; i++){
+       if(s[i] == '{'){
+        stack.push('}')
+       } else if (s[i] == '['){
+        stack.push(']');
+       } else if (s[i] == '('){
+        stack.push(')')
+       } else if(
+        stack.pop() != s[i]){
+            return false
         }
-    
-    
-    const person = new Person();
-    person.printName();
-    person.printGender();
+       
 
+       console.log(stack)
 
-    // Spread Operator / Rest
-
-    //Spread Operator is used to split up array elements OR object properties
-
-    // const newArray = [...oldArray, 1, 2];
-    // const newObject = {...oldObject, newProp:5}
-
-    //Rest Operator Used to merge a list of functions arguments into an array
-
-    sortArgs = (...args) =>{
-        return args.sort()
-    }
-
-    const numbers =[1,2,3,4];
-    const newNumbers = [...numbers,5,6,7];
-
-    console.log(newNumbers)
-
-const filter =(...args) =>{
-    return args.filter(el => el === 1);
 }
 
-console.log(filter(1,2,3))
+};
 
-//Destructuring extract array elements or objext properties and store them in variables
-
-const number = [1,2,3];
-
-[num1, ,num3] = number;
-
-console.log(num1, num3)
-
-//Array Method
-
-const numbers1 = [1,2,3,4];
-
-const doubleNumArray = numbers.map((num) => {
-    return num * 2;
-})
-
-console.log(numbers1);
-
-console.log(doubleNumArray)
-
-//Your task is to add the missing logic to a transformToObjects() function that transforms a list of numbers into a list of JavaScript objects.     
-//For the provided input [1, 2, 3] the transformToObjects() function should return [{val: 1}, {val: 2}, {val: 3}].
-function transformToObjects(numberArray) {
-    return numberArray.map((num) => {
-      return { val: num };
-    });
-  }
+console.log(isValid(bracket));
